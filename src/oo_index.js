0b5156207a1cddb5a50a07e84cbeafa42f7c9be1
Yoan Ante <yoanante@gmail.com>
2018-11-20 17:05:38 -0500

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,25 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+
+  const taskForm = document.querySelector('#create-task-form')
+  const taskUl = document.querySelector('#tasks')
+
+
+  taskForm.addEventListener('submit', function submitForm(event) {
+     event.preventDefault()
+     const userInputField = event.target.querySelector('#new-task-description')
+     const newLi = document.createElement('li')
+     taskUl.appendChild(newLi).innerText = userInputField.value
+   });
+
+   const taskLi = document.getElementsByTagName('li')
+   taskUl.addEventListener('click', function(e){
+
+     console.log(taskUl)
+     console.log(taskLi)
+
+     taskUl.removeChild(taskLi[0])
+
+   })
 });
